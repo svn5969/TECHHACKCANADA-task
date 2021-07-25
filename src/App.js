032267fee93b915834dcda7de57902e3a78a9a32
svn5969 <sourav.svn.10@gmail.com>
2021-07-26 00:00:38 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+
+    <div>
+      
+    </div>
+
+  );
+}
+
+export default App;
